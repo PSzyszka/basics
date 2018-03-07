a3aa8e19e3eaa2eb0aa5c95e28f6cdfd85f4dcfb
PSzyszka <pawelszyszka26@wp.pl>
2018-03-07 11:35:11 +0100

    Adding adjustments to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
   protect_from_forgery with: :exception
-
   helper_method :current_person
 
   private
@@ -8,5 +8,18 @@ class ApplicationController < ActionController::Base
   def current_person
     return unless session[:current_user_id].present?
     @current_person ||= Account.find(session[:current_user_id]).owner
+  end
+
+  def logged_in?
+    current_person != nil
+  end
+
+  def require_login
+    unless logged_in?
+      session[:return_to] ||= request.original_url
+
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path # halts request cycle
+    end
   end
 end
