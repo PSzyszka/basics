75bd31d24fdd5c828d8a2e3b3bc2c985550e3bd2
PSzyszka <pawelszyszka26@wp.pl>
2018-03-12 22:17:59 +0100

    Updating account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,15 @@
 class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   attr_accessor :password_confirmation, :password
 
   belongs_to :owner, class_name: 'Person', optional: true, foreign_key: :owner_id
   accepts_nested_attributes_for :owner
 
   validates :email, presence: true
-
-  # has_secure_password
 
   def self.authenticate(email, password)
     find_by(email: email).try(:authenticate, password)
